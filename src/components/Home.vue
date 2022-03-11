4cb72bba206277abe5af425440e19092f4410deb
Sepush <sepush@outlook.com>
2022-03-12 03:39:06 +0800

    feat: item

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 import { NLayout, NSpace } from 'naive-ui'
 import Header from './Header.vue'
+import Item from './Item.vue'
 </script>
 
 <template>
   <NSpace>
     <NLayout>
-      <Header />
+      <div class="page">
+        <Header />
+        <Item />
+      </div>
     </NLayout>
   </NSpace>
 </template>
+
+<style scoped>
+.page {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  text-align: center;
+}
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { NButton, NIcon, NImage, NSpace } from 'naive-ui'
+import { WeiboOutlined } from '@vicons/antd'
+</script>
+
+<template>
+  <n-space vertical>
+    <n-image
+      width="100"
+      src=" https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
+    />
+    <n-button
+      text
+      tag="a"
+      href="https://anyway.fm/news.php"
+      target="_blank"
+    >
+      <template #icon>
+        <n-icon :component="WeiboOutlined" />
+      </template>
+      查看原微博
+    </n-button>
+  </n-space>
+</template>
