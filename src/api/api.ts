31fcd8c6bf51d371edca125640179f7858688836
Sepush <sepush@outlook.com>
2022-03-19 17:26:43 +0800

    fix: set producer

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,14 @@
 import { useFetch } from '@vueuse/core'
 import type { PicInfo, Producer, ProducerAdd } from '../types/index'
 
-const baseUrl = import.meta.env.VITE_API_HOSTNAME
+const baseUrl = import.meta.env.VITE_API_HOSTNAME || 'http://awsl-py.dev.jcstaff.club'
 
 function handleFetchError() {
   window.$message.error('不可以色色哟！')
 }
 
 export async function getProducers() {
-  const { data, onFetchError } = await useFetch<Producer[]|null>(
+  const { data, onFetchError } = await useFetch<Producer[] | null>(
     `${baseUrl}/producers`,
   ).get().json()
   onFetchError(handleFetchError)
@@ -19,14 +19,9 @@ export async function getProducers() {
 export async function setProducers(producer: ProducerAdd) {
   const { data } = await useFetch<boolean>(
     `${baseUrl}/producers`,
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        uid: producer.uid,
-        keyword: producer.keyword,
-      }),
-    },
-  )
+  ).post(
+    producer,
+  ).json()
   if (data !== null)
     return data.value
 }
