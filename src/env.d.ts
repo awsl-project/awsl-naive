e34e8e9ace8be1a63ccd235e7b845f43b4b142cc
Sepush <sepush@outlook.com>
2024-05-03 14:25:13 +0800

    chore: update deps

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,12 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
+
+  const component: DefineComponent<
+    Record<string, any>,
+    Record<string, any>,
+    any
+  >
   export default component
 }
 
